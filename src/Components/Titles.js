7544cc7e591ff7b3a95e6b9fa733b0a2bd217315
luisop <luisfop77@gmail.com>
2019-05-03 16:56:09 -0300

    basic project start

---
+++ src/Components/Titles.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Titles extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Wheather Finder</h1>
+                <p>FInd out tempearute,conditions and more...</p>
+            </div>
+        )
+    }
+}
+
+
+
+export default Titles;
---
+++ src/Components/Wheather.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Wheather extends React.Component {
+    render(){
+        return( 
+
+            <div>
+                <p>This is my wheather component</p>
+            </div>
+
+        )
+    }
+}
+
+
+
+export default Wheather;
